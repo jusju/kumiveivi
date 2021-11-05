94ed953a1dc732f002b1fd48ca0f6152f97c6ae0
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2021-11-05 13:08:34 +0200

    Update sketch_nov05a.ino

--- sketch_nov05a.ino
+++ sketch_nov05a.ino
@@ -1,6 +1,18 @@
+/* I2C LCD with Arduino example code. More info: https://www.makerguides.com */
 
+// Include the libraries:
+// LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
+
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.
 
 void setup() {
+  // Initiate the LCD:
+  lcd.init();
+  lcd.backlight();
   //start serial connection
   Serial.begin(9600);
   //configure pin 2 as an input and enable the internal pull-up resistor
@@ -9,13 +21,14 @@ void setup() {
 }
 
 void loop() {
-  //read the pushbutton value into a variable
+  // Print 'Hello World!' on the first line of the LCD:
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("Hello World!"); // Print the string "Hello World!"
+  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("KUMIVEIVI");
   int sensorVal = digitalRead(2);
-  //print out the value of the pushbutton
   Serial.println(sensorVal);
-  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
-  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
-  // button's pressed, and off when it's not:
+
   if (sensorVal == HIGH) {
     digitalWrite(13, LOW);
   } else {
